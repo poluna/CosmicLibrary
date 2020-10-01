aff4775cfaa576dbddb3e68afeeb275cc9b325f6
Pola <polaskibinska@gmail.com>
2020-10-01 19:09:12 +0200

    Relocate data from an array to database (MongoDB)

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
-
+const customers = require("./routes/customers");
 const home = require("./routes/home");
 const app = express();
 
+mongoose
+  .connect("mongodb://localhost/dbtest")
+  .then(() => console.log("Connect to MongoDB..."))
+  .catch((err) => console.log("Could not connect to MongoDB..."));
+
 app.use(express.json());
 app.use("/api/genres", genres); // for any routes that starts with "/api/genres" use this router (genres)
+app.use("/api/customers", customers);
 app.use("/", home);
 
 const port = process.env.PORT || 3000;
